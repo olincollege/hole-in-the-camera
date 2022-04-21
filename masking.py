76d28531f086a6e99893be5707148cdc99d29026
Anmol Sandhu <anmolrattansandhu@gmail.com>
2022-04-21 13:25:06 -0400

    added pygame integration

--- masking.py
+++ masking.py
@@ -7,19 +7,28 @@
 # frame = cv.Canny(frame,100,150)    #---> edge detection
 import cv2 as cv
 import numpy as np
+import pygame
 
 cap = cv.VideoCapture(0)
+pygame.init()
 while True:
     isTrue, frame = cap.read()
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # frame = cv.GaussianBlur(frame, (5, 5), cv.BORDER_DEFAULT)
-    ret, thresh = cv.threshold(frame, 50, 100, cv.THRESH_BINARY)
+    # ret, thresh = cv.threshold(frame, 50, 150, cv.THRESH_BINARY)
+    ret, thresh = cv.threshold(
+        frame, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
     blank = np.zeros(frame.shape[:2], dtype="uint8")
     countours, hierarchies = cv.findContours(
         thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
     cv.drawContours(blank, countours, -1, (0, 255, 0), 1)
     frame = cv.Canny(frame, 200, 250)
-    cv.imshow("video", frame)
+    # cv.imshow("video", thresh)
+    screen = pygame.display.set_mode((thresh.shape[1], thresh.shape[0]))
+    new_surf = pygame.transform.rotate(
+        pygame.surfarray.make_surface(frame), -90)
+    screen.blit(new_surf, (0, 0))
+    pygame.display.update()
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
 
