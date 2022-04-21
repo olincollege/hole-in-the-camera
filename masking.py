57de72feb060f756aa52028bbb05d83909707568
Anmol Sandhu <anmolrattansandhu@gmail.com>
2022-04-21 16:12:16 -0400

    added white color mask

--- masking.py
+++ masking.py
@@ -5,6 +5,7 @@
 # frame = cv.GaussianBlur(frame,(1,1),cv.BORDER_DEFAULT)     ---> blur
 #ret, frame = cv.threshold(frame,100,155, cv.THRESH_BINARY)
 # frame = cv.Canny(frame,100,150)    #---> edge detection
+# (0, 0, 200), (145, 60, 255)
 import cv2 as cv
 import numpy as np
 import pygame
@@ -13,20 +14,18 @@ cap = cv.VideoCapture(0)
 pygame.init()
 while True:
     isTrue, frame = cap.read()
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(frame, (0, 35, 0), (210, 255, 255))
+    mask = cv.erode(mask, None, iterations=2)
+    mask = cv.dilate(mask, None, iterations=2)
+    cnts, hier = cv.findContours(mask.copy(), cv.RETR_EXTERNAL,
+                                 cv.CHAIN_APPROX_SIMPLE)
     frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # frame = cv.GaussianBlur(frame, (5, 5), cv.BORDER_DEFAULT)
-    # ret, thresh = cv.threshold(frame, 50, 150, cv.THRESH_BINARY)
-    ret, thresh = cv.threshold(
-        frame, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
-    blank = np.zeros(frame.shape[:2], dtype="uint8")
-    countours, hierarchies = cv.findContours(
-        thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(blank, countours, -1, (0, 255, 0), 1)
-    frame = cv.Canny(frame, 200, 250)
-    # cv.imshow("video", thresh)
-    screen = pygame.display.set_mode((thresh.shape[1], thresh.shape[0]))
+    cv.drawContours(frame, cnts, -1, (0, 255, 0), 3)
+    # cv.imshow("video", mask)
+    screen = pygame.display.set_mode((mask.shape[1], mask.shape[0]))
     new_surf = pygame.transform.rotate(
-        pygame.surfarray.make_surface(frame), -90)
+        pygame.surfarray.make_surface(mask), -90)
     screen.blit(new_surf, (0, 0))
     pygame.display.update()
     if cv.waitKey(20) & 0xFF == ord('d'):
