ae8155399d02b84f356a51c597c71c22000abf66
Anmol Sandhu <anmolrattansandhu@gmail.com>
2022-04-30 14:05:44 -0400

    refined masking process

--- masking.py
+++ masking.py
@@ -6,7 +6,6 @@
 #ret, frame = cv.threshold(frame,100,155, cv.THRESH_BINARY)
 # frame = cv.Canny(frame,100,150)    #---> edge detection
 # (0, 0, 200), (145, 60, 255)
-from itertools import count
 import cv2 as cv
 from cv2 import compare
 import numpy as np
@@ -15,25 +14,29 @@ import pygame
 cap = cv.VideoCapture(0)
 # pygame.init()
 compare_mask = cv.imread("mask.png")
+isTrue, frame = cap.read()
+compare_mask = cv.resize(compare_mask, (frame.shape[1], frame.shape[0]))
 while True:
     isTrue, frame = cap.read()
     frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     mask = cv.inRange(frame, (0, 35, 0), (210, 255, 255))
-    mask = cv.erode(mask, None, iterations=2)
-    mask = cv.dilate(mask, None, iterations=2)
-    # cnts, hier = cv.findContours(mask.copy(), cv.RETR_EXTERNAL,
-    #                              cv.CHAIN_APPROX_SIMPLE)
+    mask = cv.erode(mask, None, iterations=10)
+    mask = cv.dilate(mask, None, iterations=10)
+    cnts, hier = cv.findContours(mask.copy(), cv.RETR_EXTERNAL,
+                                 cv.CHAIN_APPROX_SIMPLE)
     # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # cv.drawContours(frame, cnts, -1, (0, 255, 0), 3)
     frame = cv.cvtColor(frame, cv.COLOR_HSV2BGR)
-    dst = cv.bitwise_and(frame, compare_mask)
+    mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+    cv.drawContours(mask, cnts, -1, (0, 255, 0), 3)
+    dst = cv.bitwise_and(frame, mask)
     cv.imshow("video", dst)
     # screen = pygame.display.set_mode((mask.shape[1], mask.shape[0]))
     # new_surf = pygame.transform.rotate(
     #     pygame.surfarray.make_surface(mask), -90)
     # screen.blit(new_surf, (0, 0))
     # pygame.display.update()
-    if cv.waitKey(20) & 0xFF == ord('d'):
+    if cv.waitKey(1) & 0xFF == ord('d'):
+        # cv.imwrite("new_mask.png", mask)
         compare_mask = cv.cvtColor(compare_mask, cv.COLOR_BGR2GRAY)
         count = 0
         for i in range(0, mask.shape[0]):
