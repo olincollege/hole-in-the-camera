00f20d66d82bc56a68ed8dc4a5bfb98fcaced39c
achakraborty <achakraborty@olin.edu>
2022-05-08 14:00:50 -0400

    broke create scripts into helper functions

--- create_csv.py
+++ create_csv.py
@@ -17,31 +17,57 @@ MASK_NAMES = [
     "seventh_mask",
 ]
 
+def analyze_image(image_name):
+    """
+    This function analyzes a given image and returns the joint positions
+    found within it as a dictionary.
+
+    Args:
+        image_name (str): The name of the image to analyze.
+    Returns:
+        joint_positions (dict): A dictionary where each key is a string number
+            corresponding to a joint and each value is the pixel location of
+            the joint in the image, [-1, -1] if it is not found.
+    """
+    image = cv2.imread(f"images/poses/{image_name}.png")
+    candidate, subset = BODY_ESTIMATION(image)
+
+    joint_positions = {}
+    for index, value in enumerate(subset[0]):
+        if value >= 0:
+            joint_positions[f"{index}"] = [
+                candidate[int(value)][0],
+                candidate[int(value)][1],
+            ]
+        else:
+            joint_positions[f"{index}"] = [-1, -1]
+        if index >= 17:
+            break
+    return joint_positions
+
+def write_to_csv(csv_name, joint_positions):
+    """
+    This function writes the found joint positions to a csv file with the same
+    name.
+
+    Args:
+        csv_name (str): The name of the new csv file.
+        joint_positions (dict): A dictionary where each key is a string number
+            corresponding to a joint and each value is the pixel location of
+            the joint in the image, [-1, -1] if it is not found.
+    """
+    with open(f"mask_joint_positions/{csv_name}.csv", "w") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            for key, value in joint_positions.items():
+                csv_writer.writerow([key, value[0], value[1]])
+
 def main():
     """
     This is the main runner function to create csv files.
     """
     for file_name in MASK_NAMES:
-        image = cv2.imread(f"images/poses/{file_name}.png")
-        candidate, subset = BODY_ESTIMATION(image)
-
-        joint_positions = {}
-        for index, value in enumerate(subset[0]):
-            if value >= 0:
-                joint_positions[f"{index}"] = [
-                    candidate[int(value)][0],
-                    candidate[int(value)][1],
-                ]
-            else:
-                joint_positions[f"{index}"] = [-1, -1]
-            if index >= 17:
-                break
-
-        with open(f"mask_joint_positions/{file_name}.csv", "w") as csv_file:
-            csv_writer = csv.writer(csv_file)
-            for key, value in joint_positions.items():
-                csv_writer.writerow([key, value[0], value[1]])
-
+        joint_positions = analyze_image(file_name)
+        write_to_csv(file_name, joint_positions)
 
 if __name__ == "__main__":
     main()
--- create_mask.py
+++ create_mask.py
@@ -1,12 +1,24 @@
 """
 This script is used to make holes for users to fit into and saves the original
-frame and the hole to iamges/poses and images/masks respectively.
+frame and the hole to images/poses and images/masks respectively.
 """
 from cv2 import cv2 as cv
 
 # Open a camera for video capturing.
 CAMERA = cv.VideoCapture(0)
-MASK_NAME = "sixth_mask"
+MASK_NAME = "smask"
+
+def analyze_camera_frame():
+    # Capture frame-by-frame
+    _, frame = CAMERA.read()
+    # convert to HSV colorspace and apply threshold
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    mask = cv.inRange(frame, (0, 14, 0), (180, 255, 255))
+    # apply image erosion and dilation to sharpen edges
+    mask = cv.erode(mask, None, iterations=5)
+    mask = cv.dilate(mask, None, iterations=5)
+    cv.imshow("video", mask)
+    return frame, mask
 
 def main():
     """
@@ -14,15 +26,7 @@ def main():
     """
 
     while True:
-        # Capture frame-by-frame
-        _, frame = CAMERA.read()
-        # convert to HSV colorspace and apply threshold
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        mask = cv.inRange(frame, (0, 14, 0), (180, 255, 255))
-        # apply image erosion and dilation to sharpen edges
-        mask = cv.erode(mask, None, iterations=5)
-        mask = cv.dilate(mask, None, iterations=5)
-        cv.imshow("video", mask)
+        frame, mask = analyze_camera_frame()
         # break if user presses 'd'
         if cv.waitKey(1) & 0xFF == ord("d"):
             # save the current frame and the mask applied on it
